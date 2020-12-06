151c043df916e72791705ff0c64965e26b67ac0f
CAH9487 <chinganhsu9487@gmail.com>
2020-12-06 13:58:23 +0800

    add 002_UploadTest (on going)

---
+++ 002_UploadTest/requestHandlers.js
@@ -0,0 +1,34 @@
+var querystring = require("querystring");
+
+function start(response, postData) {
+  console.log("Request handler 'start' was called.");
+
+  var body = '<html>\n' +
+    '    <head>\n' +
+    '        <meta http-equiv="Content-Type" content="text/html; \n' +
+    '            charset=UTF-8" />\n' +
+    '    </head>\n' +
+    '    <body>\n' +
+    '        <form action="/upload" method="post">\n' +
+    '            <textarea name="text" rows="20" cols="60"></textarea>\n' +
+    '            <br>\n' +
+    '            <input type="submit" value="Submit text" />\n' +
+    '        </form>\n' +
+    '    </body>\n' +
+    '</html>\n';
+
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.write(body);
+  response.end();
+}
+
+function upload(response, postData) {
+  console.log("Request handler 'upload' was called.");
+  console.log(`\tpostData = ${postData}`);
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.write(`You've sent the text: ${querystring.parse(postData).text}`);
+  response.end();
+}
+
+exports.start = start;
+exports.upload = upload;
---
+++ 002_UploadTest/server.js
@@ -0,0 +1,26 @@
+var http = require("http");
+var url = require("url");
+
+// http://localhost:8080/
+function start(route, handle) {
+    http.createServer(function (request, response) {
+        var postData = "";
+        var pathname = url.parse(request.url).pathname;
+        console.log(`Request for ${pathname} received.`);
+
+        request.setEncoding("utf8");
+
+        request.addListener("data", (postDataChunk) => {
+            postData += postDataChunk;
+            console.log(`Received POST data chunk ${postDataChunk}.`);
+        });
+
+        request.addListener("end", () => {
+            route(handle, pathname, response, postData);
+        });
+    }).listen(8080);
+
+    console.log("Server has started.");
+}
+
+exports.start = start
